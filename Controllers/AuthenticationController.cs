1578eeec8beac3ec1cb2012b9d1dbea324933d8d
fiiuliana <fiiulianaf@gmail.com>
2021-06-26 14:29:50 +0300

    Adding Auth controller Adding Swagger comments

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using CentrulMultimedia.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentrulMultimedia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AuthenticationController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
